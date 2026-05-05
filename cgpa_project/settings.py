1a4ffa5117bacdcca69f541c70711f2fd515d20c
estheroluwatosin817-dot <estheroluwatosin817@gmail.com>
2026-05-05 21:13:33 +0100

    Fix ALLOWED_HOSTS for Render deployment

--- cgpa_project/settings.py
+++ cgpa_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'pXt_yOOrCz_qsb-mVo09TrME2vXq6ljKsNERa
 # 🔴 PRODUCTION FIX
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,cgpacal-lpdn.onrender.com').split(',')
 
 
 INSTALLED_APPS = [
